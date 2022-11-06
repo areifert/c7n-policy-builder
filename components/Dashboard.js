dc90f79293915ed77c1f169f3f6969adf20d85a4
areifert <areifert@users.noreply.github.com>
2022-11-06 09:28:47 -0700

    Submitting one possible format

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import { Badge, Box, Container, CssBaseline, Divider, Grid, IconButton,
-  Link, List, Paper, Toolbar, Typography, } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Badge, Box, Checkbox, Container, CssBaseline, Divider, FormGroup, FormControlLabel, Grid, IconButton,
+  Link, List, ListItemButton, ListItemText, ListSubheader, Paper, Toolbar, Typography, } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import SchemaItems from './SchemaItems';
+import { commonActions, commonFilters, getServiceConfig, SchemaItems } from './SchemaItems';
 
 function Copyright(props) {
   return (
@@ -139,6 +140,26 @@ function DashboardContent() {
               <ChevronLeftIcon />
             </IconButton>
           </Toolbar>
+          {selectedServices.length > 0 &&
+            <React.Fragment>
+              <Divider />
+              <List component="nav">
+                <ListSubheader component="div" inset>
+                  Selected services
+                </ListSubheader>
+                {selectedServices.map((service, index) => (
+                  <ListItemButton key={index} onClick={() => {
+                    setSelectedServices((services) => {
+                      services.splice(services.findIndex((value) => value === service), 1)
+                      return Array.from(services);
+                    });
+                  }}>
+                    <ListItemText primary={service} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </React.Fragment>
+          }
           <Divider />
           <List component="nav">
             <SchemaItems setSelectedServices={setSelectedServices} />
@@ -159,20 +180,75 @@ function DashboardContent() {
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-              {selectedServices.map((service, index) => (
-                <Grid item xs={12} md={4} lg={3} key={index}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      height: 240,
-                    }}
-                  >
-                    <Typography variant="overline">{service}</Typography>
-                  </Paper>
-                </Grid>
-              ))}
+              {selectedServices.map((service, index) => {
+                const serviceConfig = getServiceConfig(service);
+
+                return (
+                  <Grid item xs={12} md={6} key={index}>
+                    <Paper
+                      sx={{
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                      }}
+                    >
+                      <Typography variant="overline">{service}</Typography>
+
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                        >
+                          <Typography sx={{ width: '33%', flexShrink: 0 }}>Actions</Typography>
+                          <Typography sx={{ color: 'text.secondary' }}>0 selected</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <FormGroup>
+                            {Object.keys(serviceConfig.actions).map((action, actionIndex) => (
+                              <FormControlLabel key={actionIndex} control={<Checkbox />} label={action} />
+                              // <Typography key={actionIndex}>{action}</Typography>
+                            ))}
+                            <Divider>Common Actions</Divider>
+                            {commonActions.map((action, actionIndex) => (
+                              <FormControlLabel key={actionIndex} control={<Checkbox />} label={action} />
+                              // <Typography key={actionIndex}>{action}</Typography>
+                            ))}
+                          </FormGroup>
+                        </AccordionDetails>
+                      </Accordion>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                        >
+                          <Typography sx={{ width: '33%', flexShrink: 0 }}>Filters</Typography>
+                          <Typography sx={{ color: 'text.secondary' }}>0 selected</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <FormGroup>
+                            {Object.keys(serviceConfig.filters).map((filter, filterIndex) => (
+                              <FormControlLabel key={filterIndex} control={<Checkbox />} label={filter} />
+                            ))}
+                            <Divider>Common Filters</Divider>
+                            {commonFilters.map((filter, filterIndex) => (
+                              <FormControlLabel key={filterIndex} control={<Checkbox />} label={filter} />
+                            ))}
+                          </FormGroup>
+                        </AccordionDetails>
+                      </Accordion>
+
+                      {/* <Divider>Actions</Divider>
+                      {Object.keys(serviceConfig.actions).concat(commonActions).map((action, actionIndex) => (
+                        <Chip key={actionIndex} label={action} variant="outlined" onClick={() => {console.log('click')}} />
+                        // <Typography key={actionIndex}>{action}</Typography>
+                      ))} */}
+
+                      {/* <Divider>Filters</Divider>
+                      {Object.keys(serviceConfig.filters).concat(commonFilters).map((filter, filterIndex) => (
+                        <Typography key={filterIndex} variant="caption">{filter}</Typography>
+                      ))} */}
+                    </Paper>
+                  </Grid>
+                );
+              })}
             </Grid>
             <Copyright sx={{ pt: 4 }} />
           </Container>
--- components/SchemaItems.js
+++ components/SchemaItems.js
@@ -6,7 +6,14 @@ import ListSubheader from '@mui/material/ListSubheader';
 import c7n_schema from '../public/c7n-schema.json';
 
 
-export default function SchemaItems(props) {
+export const commonActions = Object.keys(c7n_schema.definitions.actions);
+export const commonFilters = Object.keys(c7n_schema.definitions.filters);
+
+export function getServiceConfig(service_name) {
+  return c7n_schema.definitions.resources[service_name];
+}
+
+export function SchemaItems(props) {
   const { setSelectedServices } = props;
 
   return (
@@ -15,7 +22,7 @@ export default function SchemaItems(props) {
         Available services
       </ListSubheader>
       {Object.keys(c7n_schema.definitions.resources).map((value, index) => (
-        <ListItemButton key={index} onClick={(e) => {
+        <ListItemButton key={index} onClick={() => {
           setSelectedServices((services) => {
             if (!services.includes(value)) {
               services.push(value);
